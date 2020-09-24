f7ddbc4bce9e0fca95917e009c606e91ffdd4a87
Joel Fernando <joeldomingueztapia@gmail.com>
2020-09-24 09:19:04 -0700

    Shop List added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component.jsx';
-
-const HatsPage = ({ history }) => {
-  return(
-    <div>
-      <h1>HATS PAGE</h1>
-      <p onClick={() => history.push('/')}>Return to Homepage</p>
-    </div>
-  )
-};
+import ShopPage from './pages/shop/shop.component.jsx'
 
 function App() {
   return (
-    <div>
+    <Switch>
         <Route exact path='/' component={Homepage}/>
-        <Route path='/hats' component={HatsPage}/>
-    </div>
+        <Route path='/shop' component={ShopPage}/>
+    </Switch>
   );
 }
 
