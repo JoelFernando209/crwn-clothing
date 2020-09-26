ecde2c262fa12a654677daba37e76092a91e34aa
Joel Fernando <joeldomingueztapia@gmail.com>
2020-09-26 07:41:44 -0700

    Added SignUp Simple Form, added SignInAndUp page with Route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import Homepage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
+import SignInAndUpPage from './pages/sign-in-and-up/sign-in-and-up.component.jsx';
 import Header from './components/header/header.component.jsx';
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
     <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndUpPage}/>
+
     </Switch>
     </>
   );
