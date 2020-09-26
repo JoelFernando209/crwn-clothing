b0f142d39439c0fab6d35f27090c054110facd8e
Joel Fernando <joeldomingueztapia@gmail.com>
2020-09-26 06:36:38 -0700

    Adding header component and LLink to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component.jsx';
-import ShopPage from './pages/shop/shop.component.jsx'
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
+    <>
+    <Header />
     <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route path='/shop' component={ShopPage}/>
     </Switch>
+    </>
   );
 }
 
