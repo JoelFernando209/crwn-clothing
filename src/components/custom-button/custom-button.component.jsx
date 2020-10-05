bf82c33d778d9440e6d56638cc3239c11bb69abf
JoelFernando209 <joeldomingueztapia@gmail.com>
2020-10-05 13:45:35 +0000

    updated shop data with quantity functionality

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+const CustomButton = ({ children, isGoogleSignIn, inverted,...otherProps }) => (
   <button
   type='submit'
-  className={`${isGoogleSignIn ? 'google-sign-in':''} custom-button`} {...otherProps }
+  className={`${isGoogleSignIn ? 'google-sign-in':''} ${inverted ? 'inverted':''}  custom-button`} {...otherProps }
   >
     {children}
   </button>
